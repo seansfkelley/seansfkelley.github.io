c7c181f1935ba765c2b4506ead1ac5d677b5275b
CircleCI Deploy Task <circleci-deploy-task@nope.nope>
2021-05-17 22:43:27 +0000

    Automated deployment to GitHub Pages: c44e73bc51df38d9c7cfc770301636fa987f4602

--- assets/literate-recipe/literate-recipe.js
+++ assets/literate-recipe/literate-recipe.js
@@ -1,17 +1,31 @@
 (function () {
-  // Note that this has to be const, otherwise scope capture won't work properly in onClick.
-  for (const e of document.querySelectorAll("button[data-step-number]")) {
-    e.style.display = null;
-    e.textContent = "mark as done";
-    e.addEventListener("click", () => {
-      const stepNumber = +e.dataset.stepNumber;
-      const stepHeader = document.getElementById(`step-header-${stepNumber}`);
-      stepHeader.classList.add("complete");
-      stepHeader.classList.remove("up-next");
+  const checkboxContainer = document.getElementById("interactive-checkbox");
+  checkboxContainer.style.display = null;
 
-      const nextStepHeader = document.getElementById(`step-header-${stepNumber + 1}`);
-      if (nextStepHeader) {
-        nextStepHeader.classList.add("up-next");
+  const article = document.querySelector("article");
+  const checkbox = checkboxContainer.querySelector("input");
+
+  checkbox.addEventListener("change", (e) => {
+    article.classList.toggle("interaction-enabled", e.target.checked);
+  });
+
+  // Since the checkbox has an ID, soft reloads of the page will keep it checked if it was before.
+  if (checkbox.checked) {
+    article.classList.add("interaction-enabled");
+  }
+
+  document.querySelector("section.recipe-step").classList.add("current");
+
+  // Note that this has to be const, otherwise scope capture won't work properly in onClick.
+  for (const step of document.querySelectorAll("section.recipe-step")) {
+    const stepNumber = +step.dataset.stepNumber;
+    step.querySelector("button.done-button").addEventListener("click", () => {
+      step.classList.remove("current");
+      step.classList.add("complete");
+
+      const nextStep = document.querySelector(`section.recipe-step[data-step-number="${stepNumber + 1}"]`);
+      if (nextStep) {
+        nextStep.classList.add("current");
       }
     });
   }
