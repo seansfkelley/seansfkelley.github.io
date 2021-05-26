b9c28037179623b6dbcdfa3a4961f9d20bc43256
CircleCI Deploy Task <circleci-deploy-task@nope.nope>
2021-05-26 18:30:26 +0000

    Automated deployment to GitHub Pages: 88274b7c613c0859aa8707b7c7218f2009606e69

--- assets/literate-recipe/literate-recipe.js
+++ assets/literate-recipe/literate-recipe.js
@@ -1,32 +1,75 @@
 (function () {
-  const checkboxContainer = document.getElementById("interactive-checkbox");
-  checkboxContainer.style.display = null;
+  class Duration {
+    constructor(minutes) {
+      this.minutes = minutes;
+    }
 
-  const article = document.querySelector("article");
-  const checkbox = checkboxContainer.querySelector("input");
+    static fromDate(date, roundUpTo = 1) {
+      const minutes = date.getHours() * 60 + date.getMinutes();
+      return new Duration(Math.ceil(minutes / roundUpTo) * roundUpTo);
+    }
 
-  checkbox.addEventListener("change", (e) => {
-    article.classList.toggle("interaction-enabled", e.target.checked);
-  });
+    static fromTimeInputValue(string) {
+      const [hours, minutes] = string.split(":");
+      return new Duration(+hours * 60 + +minutes);
+    }
 
-  // Since the checkbox has an ID, soft reloads of the page will keep it checked if it was before.
-  if (checkbox.checked) {
-    article.classList.add("interaction-enabled");
+    static _toHoursAndMinutes(minutes) {
+      return [Math.floor(minutes / 60), minutes % 60];
+    }
+
+    toTimeInputValue() {
+      const [hours, minutes] = Duration._toHoursAndMinutes(
+        (this.minutes < 0 ? this.minutes + 24 * 60 : this.minutes) % (24 * 60)
+      );
+      return `${hours < 10 ? "0" : ""}${hours}:${
+        minutes < 10 ? "0" : ""
+      }${minutes}`;
+    }
   }
 
-  document.querySelector("section.recipe-step").classList.add("current");
+  const steps = [];
 
-  // Note that this has to be const, otherwise scope capture won't work properly in onClick.
-  for (const step of document.querySelectorAll("section.recipe-step")) {
-    const stepNumber = +step.dataset.stepNumber;
-    step.querySelector("button.done-button").addEventListener("click", () => {
-      step.classList.remove("current");
-      step.classList.add("complete");
-
-      const nextStep = document.querySelector(`section.recipe-step[data-step-number="${stepNumber + 1}"]`);
-      if (nextStep) {
-        nextStep.classList.add("current");
-      }
+  function recalculateOffsetsRelativeTo(stepIndex) {
+    const selectedTime = Duration.fromTimeInputValue(
+      steps[stepIndex].timeInput.value
+    );
+    steps.forEach(({ offset, timeInput }) => {
+      timeInput.value = new Duration(
+        selectedTime.minutes + offset - steps[stepIndex].offset
+      ).toTimeInputValue();
     });
   }
+
+  const nowDuration = Duration.fromDate(new Date(), 5);
+
+  document.querySelectorAll(".recipe-step").forEach((step, index) => {
+    const offset = (steps[steps.length - 1]?.offset ?? 0) + +step.dataset.wait;
+
+    const timeInput = document.createElement("input");
+    timeInput.type = "time";
+    timeInput.step = "300";
+    timeInput.value = nowDuration.toTimeInputValue();
+    timeInput.addEventListener("change", (e) => {
+      recalculateOffsetsRelativeTo(index);
+    });
+
+    const setToNowButton = document.createElement("input");
+    setToNowButton.type = "button";
+    setToNowButton.value = "set to now";
+    setToNowButton.addEventListener("click", () => {
+      timeInput.value = Duration.fromDate(new Date(), 5).toTimeInputValue();
+      recalculateOffsetsRelativeTo(index);
+    });
+
+    step.querySelector(".metadata").appendChild(setToNowButton);
+    step.querySelector(".metadata").appendChild(timeInput);
+
+    steps.push({
+      offset,
+      timeInput,
+    });
+  });
+
+  recalculateOffsetsRelativeTo(0);
 })();
