889c6bf5fa85af99a96997380d4dd35975063569
CircleCI Deploy Task <circleci-deploy-task@nope.nope>
2023-08-12 01:26:51 +0000

    Automated deployment to GitHub Pages: 17643280e1c1df01904be9ff8f84e219c985f1cc
    Commit title: Switch to using Yarn.

--- assets/blazonry/blazonry.js
+++ assets/blazonry/blazonry.js
@@ -12,6 +12,7 @@ TODO
 - what is the CSS to make line-stroke not scale? (apply that to the quartering lines so they are always 1 pixel)
 - fretty?
 - why is a chevron embattled/indented appear to be vertically shifted, but engrailed does not? (or does it?)
+  - "per pale wavy argent and purpure" is definitely off-center
 - "saltirewise" needs to vary based on where the charge is
 - "embattled" shouldn't do the bottom of chevrons and fesses; that's embattled-counter-embattled
 - more of the same
@@ -1518,10 +1519,12 @@ function parseAndRenderBlazon() {
         if (results.length === 0) {
             error.style.display = "block";
             error.innerHTML = "Unexpected end of input.";
+            return;
         }
         else if (results.length > 1) {
             error.style.display = "block";
             error.innerHTML = "Ambiguous blazon!";
+            return;
         }
         else {
             result = recursivelyOmitNullish(results[0]);
