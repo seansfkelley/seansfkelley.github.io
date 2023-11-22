93f0689fd37e4c8bedd7d8622eb968719b68c19c
CircleCI Deploy Task <circleci-deploy-task@nope.nope>
2023-11-22 23:22:11 +0000

    Automated deployment to GitHub Pages: 368839bb481aac784d2978ef006c9831d98df7f4
    Commit title: Add linked from tagged posts to the tag page.

--- assets/js/theme-switcher.js
+++ assets/js/theme-switcher.js
@@ -1,20 +1,18 @@
-(function () {
-  if (["light", "dark"].includes(localStorage["theme"])) {
-    document.body.dataset.theme = localStorage["theme"];
+if (["light", "dark"].includes(localStorage["theme"])) {
+  document.body.dataset.theme = localStorage["theme"];
+}
+
+document.getElementById("theme-switcher-wrapper").style.display = null;
+
+document.getElementById("theme-switcher").addEventListener("click", (e) => {
+  if (document.body.dataset.theme === "auto") {
+    document.body.dataset.theme = localStorage["theme"] = "light";
+  } else if (document.body.dataset.theme === "light") {
+    document.body.dataset.theme = localStorage["theme"] = "dark";
+  } else {
+    document.body.dataset.theme = localStorage["theme"] = "auto";
   }
 
-  document.getElementById("theme-switcher-wrapper").style.display = null;
-
-  document.getElementById("theme-switcher").addEventListener("click", (e) => {
-    if (document.body.dataset.theme === "auto") {
-      document.body.dataset.theme = localStorage["theme"] = "light";
-    } else if (document.body.dataset.theme === "light") {
-      document.body.dataset.theme = localStorage["theme"] = "dark";
-    } else {
-      document.body.dataset.theme = localStorage["theme"] = "auto";
-    }
-
-    // Don't scroll to top.
-    e.preventDefault();
-  });
-})();
+  // Don't scroll to top.
+  e.preventDefault();
+});
