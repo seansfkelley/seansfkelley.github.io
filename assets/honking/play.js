e1d9e2aa6172ebdc3180e3e63c3432df1be6d903
CircleCI Deploy Task <circleci-deploy-task@nope.nope>
2024-04-18 06:42:02 +0000

    Automated deployment to GitHub Pages: 5ebecd0dc51a67b55970360db4ea6ba1aac61fa0
    Commit title: publish

---
+++ assets/honking/play.js
@@ -0,0 +1,45 @@
+const audios = [...document.getElementsByTagName("audio")];
+const mute = document.getElementById("mute");
+
+/** @type HTMLAudioElement */
+let playing = undefined;
+let muted = false;
+
+function maybeHonk() {
+  if (muted || playing != null) {
+    return;
+  }
+
+  function onEnded() {
+    playing = undefined;
+    which.removeEventListener("ended", onEnded);
+  }
+
+  const which = audios[Math.floor(Math.random() * audios.length)];
+  which.addEventListener("ended", onEnded);
+  which.play();
+  playing = which;
+
+  // Wait until the first interaction to show it.
+  mute.style = null;
+}
+
+document.addEventListener("mousedown", maybeHonk);
+document.addEventListener("keydown", maybeHonk);
+document.addEventListener("touchstart", maybeHonk);
+
+mute.addEventListener("click", () => {
+  if (playing != null) {
+    playing.pause();
+    playing.currentTime = 0;
+    playing = undefined;
+  }
+
+  muted = !muted;
+  mute.classList.toggle("muted", muted);
+  mute.innerHTML = muted ? "bring it on" : "shut the fuck up";
+
+  if (!muted) {
+    maybeHonk();
+  }
+});
