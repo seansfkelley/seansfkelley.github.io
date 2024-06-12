0212fc0622a37949035a8dc0e9bd429e73a986fd
Sean Kelley <seansfkelley@users.noreply.github.com>
2024-06-12 16:51:56 -0400

    localStorage to keep sort/filter across near-term reloads.

--- assets/movies/movies.js
+++ assets/movies/movies.js
@@ -63,6 +63,37 @@ function updateSortAndFilter() {
     // Will move the item from its original position, thereby reordering the entire list.
     list.appendChild(e);
   }
+
+  localStorage["movie-sort-filter"] = JSON.stringify({
+    kind,
+    recommendation,
+    sort,
+    timestamp: new Date().toISOString(),
+  });
+}
+
+try {
+  const { kind, recommendation, sort, timestamp } = JSON.parse(
+    localStorage["movie-sort-filter"] || "{}"
+  );
+
+  if (
+    typeof timestamp === "string" &&
+    // If this fails to parse, it will yield NaN which will propagate and fail the conditional, so
+    // we'll end up with only defaults, which is fine.
+    new Date() - new Date(timestamp) < 24 * 60 * 60 * 1000 &&
+    typeof kind === "string" &&
+    typeof recommendation === "string" &&
+    typeof sort === "string"
+  ) {
+    // If any of these are invalid, the browser will just default to what the DOM says to, so no
+    // need to be too clever.
+    form.elements.kind.value = kind;
+    form.elements.recommendation.value = recommendation;
+    form.elements.sort.value = sort;
+  }
+} catch (e) {
+  console.error("not setting default sort/filter values due to error", e);
 }
 
 form.style = null;
