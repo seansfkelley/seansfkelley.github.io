86229f881ca909ef36afc5f1c47eab5ea1ed52e8
Sean Kelley <seansfkelley@users.noreply.github.com>
2025-04-14 16:05:07 -0700

    Add a real don't walk symbol.

--- assets/intersections/intersections.ts
+++ assets/intersections/intersections.ts
@@ -18,13 +18,14 @@ interface SvgRect {
 }
 
 const SvgRect = {
-  of: (
-    x: number,
-    y: number,
-    width: number,
-    height: number,
-    rx?: number
-  ): SvgRect => ({ type: "rect", x, y, width, height, rx }),
+  of: (x: number, y: number, width: number, height: number, rx?: number): SvgRect => ({
+    type: "rect",
+    x,
+    y,
+    width,
+    height,
+    rx,
+  }),
 };
 
 interface SvgPath {
@@ -67,6 +68,11 @@ const WALK_PATHS = [
   "m 10.29 3.54 a 1.73 1.73 0 1 1 1.71 -1.74 a 1.73 1.73 0 0 1 -1.73 1.73",
   // body
   "m 3 12 c 0.54 -1.38 0.85 -2.75 1.36 -4.32 l 2 -0.62 l -6.27 17.06 l 1.85 0.66 l 5 -10.14 l 2.66 4.59 l 0.51 5.73 l 2.18 -0.11 l -0.4 -6.75 l -2.89 -5.56 l 1.61 -3.92 c 0.58 0.43 1.14 1.07 1.76 1.51 l 3.77 2 l 0.8 -1.22 l -3.94 -2.81 l -3 -4.07 l -1.48 -0.61 l -6.11 3 l -1 5.54 z",
+];
+
+// viewbox = 0 0 27 41
+const DONT_WALK_PATHS = [
+  "m 2.452 40.784 c 0.375 -0.455 0.801 -0.948 0.845 -1.749 v -5.859 c 0 -2.551 -0.861 -2.984 -1.7 -3.766 c -1.389 -1.291 -1.18 -3.121 -1.103 -5.484 c 0.213 -6.552 0.46 -10.499 0.485 -16.62 c 0.003 -0.78 0.636 -1.417 1.417 -1.417 s 1.417 0.637 1.417 1.417 l 0.486 9.934 l 0.644 -0.016 c 0.102 -4.537 0.31 -9.115 0.31 -13.65 c 0 -0.779 0.636 -1.416 1.417 -1.416 c 0.779 0 1.415 0.637 1.415 1.416 c 0 0.013 0 0.028 0.005 0.041 l 0.195 13.622 h 0.656 c 0.184 -5.14 0.551 -10.394 0.551 -15.528 c 0 -0.78 0.637 -1.416 1.415 -1.416 c 0.781 0 1.418 0.636 1.418 1.416 v 0.018 l 0.305 15.51 h 0.859 c 0.049 -4.579 0.101 -9.158 0.151 -13.737 c 0.032 -0.75 0.662 -1.342 1.414 -1.342 c 0.764 0 1.401 0.614 1.417 1.383 l 0.7 18.266 c -0.006 0.338 0.296 0.59 0.633 0.59 c 0.192 0 0.628 -0.137 0.967 -0.656 l 3.202 -4.638 c 1.212 -1.702 2.829 -2.038 4.725 -1.321 c -1.175 2.209 -3.6 7.656 -6.06 10.716 c -1.297 2.028 -5.648 4.062 -5.648 6.672 v 5.342 c 0 1.099 1.084 2.341 1.036 2.274 l -13.574 -0.002 l 0 0 z",
 ];
 
 const INTERSECTION = ((): Intersection => {
@@ -156,10 +162,7 @@ function renderPedestrianSignal(
   const SIZE = 4;
   const BUFFER = 0.5;
 
-  const outline = document.createElementNS(
-    "http://www.w3.org/2000/svg",
-    "rect"
-  );
+  const outline = document.createElementNS("http://www.w3.org/2000/svg", "rect");
   applySvgAttributes(outline, {
     class: "outline",
     x,
@@ -179,26 +182,21 @@ function renderPedestrianSignal(
     height: SIZE,
   });
 
-  const dontWalkSymbol = document.createElementNS(
-    "http://www.w3.org/2000/svg",
-    "rect"
-  );
+  const dontWalkSymbol = document.createElementNS("http://www.w3.org/2000/svg", "use");
   applySvgAttributes(dontWalkSymbol, {
     class: "symbol",
+    href: "#dont-walk-symbol",
     x: x + BUFFER,
     y: y + BUFFER,
     width: SIZE / 2,
     height: SIZE,
   });
 
-  const dontWalkText = document.createElementNS(
-    "http://www.w3.org/2000/svg",
-    "text"
-  );
+  const dontWalkText = document.createElementNS("http://www.w3.org/2000/svg", "text");
   applySvgAttributes(dontWalkText, {
     class: "text",
     // CSS sets this to be middle-end anchored, so do the math relative to that point.
-    x: x + BUFFER + SIZE + 0.5, // 0.5 = fudge factor to smash the glyph up against the right end
+    x: x + BUFFER + SIZE,
     y: y + BUFFER + SIZE / 2,
   });
 
@@ -263,10 +261,7 @@ function renderIntersection(intersection: Intersection): {
     viewBox: "0 0 100 100",
   });
 
-  const asphalt = document.createElementNS(
-    "http://www.w3.org/2000/svg",
-    "rect"
-  );
+  const asphalt = document.createElementNS("http://www.w3.org/2000/svg", "rect");
   applySvgAttributes(asphalt, {
     x: 0,
     y: 0,
@@ -276,20 +271,32 @@ function renderIntersection(intersection: Intersection): {
   });
   svg.appendChild(asphalt);
 
-  const walkSymbol = document.createElementNS(
-    "http://www.w3.org/2000/svg",
-    "symbol"
-  );
-  applySvgAttributes(walkSymbol, {
-    id: "walk-symbol",
-    viewBox: "0 0 17 25",
-  });
-  svg.append(walkSymbol);
+  {
+    const walkSymbol = document.createElementNS("http://www.w3.org/2000/svg", "symbol");
+    applySvgAttributes(walkSymbol, {
+      id: "walk-symbol",
+      viewBox: "0 0 17 25",
+    });
+    for (const d of WALK_PATHS) {
+      const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+      applySvgAttributes(path, { d });
+      walkSymbol.appendChild(path);
+    }
+    svg.append(walkSymbol);
+  }
 
-  for (const d of WALK_PATHS) {
-    const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-    applySvgAttributes(path, { d });
-    walkSymbol.appendChild(path);
+  {
+    const dontWalkSymbol = document.createElementNS("http://www.w3.org/2000/svg", "symbol");
+    applySvgAttributes(dontWalkSymbol, {
+      id: "dont-walk-symbol",
+      viewBox: "0 0 27 41",
+    });
+    for (const d of DONT_WALK_PATHS) {
+      const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+      applySvgAttributes(path, { d });
+      dontWalkSymbol.appendChild(path);
+    }
+    svg.append(dontWalkSymbol);
   }
 
   function assertCycleTimeIsCompatible(timing: CrosswalkSignalTiming): void {
@@ -297,18 +304,12 @@ function renderIntersection(intersection: Intersection): {
     if (cycleTime == null) {
       cycleTime = total;
     }
-    assert(
-      total === cycleTime,
-      "cycle time is not the same as already-defined timings"
-    );
+    assert(total === cycleTime, "cycle time is not the same as already-defined timings");
   }
 
   for (const sidewalk of intersection.sidewalks) {
     if (sidewalk.type === "rect") {
-      const rect = document.createElementNS(
-        "http://www.w3.org/2000/svg",
-        "rect"
-      );
+      const rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
 
       const { type: _type, ...rest } = sidewalk;
       applySvgAttributes(rect, { ...rest, class: "sidewalk" });
@@ -323,10 +324,7 @@ function renderIntersection(intersection: Intersection): {
 
   for (const crosswalk of intersection.crosswalks) {
     if (crosswalk.type === "line") {
-      const line = document.createElementNS(
-        "http://www.w3.org/2000/svg",
-        "line"
-      );
+      const line = document.createElementNS("http://www.w3.org/2000/svg", "line");
 
       const { type: _type, a, b } = crosswalk;
 
@@ -346,18 +344,10 @@ function renderIntersection(intersection: Intersection): {
     }
   }
 
-  for (const {
-    location,
-    timing,
-    timingOffset,
-  } of intersection.pedestrianSignals) {
+  for (const { location, timing, timingOffset } of intersection.pedestrianSignals) {
     assertCycleTimeIsCompatible(timing);
 
-    svg.appendChild(
-      recordPedestrianSignal(
-        renderPedestrianSignal(location, timing, timingOffset)
-      )
-    );
+    svg.appendChild(recordPedestrianSignal(renderPedestrianSignal(location, timing, timingOffset)));
   }
 
   return {
@@ -370,8 +360,7 @@ function renderIntersection(intersection: Intersection): {
 
 const container: SVGSVGElement = document.querySelector("#interactive")!;
 
-const { svg, pedestrianSignals, trafficSignals, cycleTime } =
-  renderIntersection(INTERSECTION);
+const { svg, pedestrianSignals, trafficSignals, cycleTime } = renderIntersection(INTERSECTION);
 container.appendChild(svg);
 
 function tick(t: number, cycleTime: number) {
