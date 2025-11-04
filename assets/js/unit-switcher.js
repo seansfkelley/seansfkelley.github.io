9d632678b9cd0a69b477b9c1d0c55bfe3609885d
CircleCI Deploy Task <circleci-deploy-task@nope.nope>
2025-11-04 22:13:40 +0000

    Automated deployment to GitHub Pages: 663d919c072beabf80c2d67c85ceab91a59d1f51
    Commit title: Complete unit switcher support for yogurt recipe and sync all tabs.

--- assets/js/unit-switcher.js
+++ assets/js/unit-switcher.js
@@ -1,5 +1,7 @@
-if (["imperial", "metric"].includes(localStorage["unit"])) {
-  document.body.dataset.unit = localStorage["unit"];
+const UNIT_KEY = "unit";
+
+if (["imperial", "metric"].includes(localStorage[UNIT_KEY])) {
+  document.body.dataset.unit = localStorage[UNIT_KEY];
 }
 
 const unitSwitcherWrapper = document.getElementById("unit-switcher-wrapper");
@@ -7,16 +9,22 @@ if (unitSwitcherWrapper != null) {
   unitSwitcherWrapper.style.display = null;
 
   document.getElementById("unit-switcher-imperial").addEventListener("click", (e) => {
-    document.body.dataset.unit = localStorage["unit"] = "imperial";
+    document.body.dataset.unit = localStorage[UNIT_KEY] = "imperial";
 
     // Don't scroll to top.
     e.preventDefault();
   });
 
   document.getElementById("unit-switcher-metric").addEventListener("click", (e) => {
-    document.body.dataset.unit = localStorage["unit"] = "metric";
+    document.body.dataset.unit = localStorage[UNIT_KEY] = "metric";
 
     // Don't scroll to top.
     e.preventDefault();
   });
 }
+
+window.addEventListener("storage", (e) => {
+  if (e.key === UNIT_KEY) {
+    document.body.dataset.unit = e.newValue;
+  }
+});
