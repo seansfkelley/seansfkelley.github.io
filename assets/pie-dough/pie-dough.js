c5dac5643116d95d25617c2254c9f7e2ab032f1e
CircleCI Deploy Task <circleci-deploy-task@nope.nope>
2025-12-04 17:21:36 +0000

    Automated deployment to GitHub Pages: 59408f5131709320f5bc8f5c139d422f481e04dd
    Commit title: Add reference images for pie.

---
+++ assets/pie-dough/pie-dough.js
@@ -0,0 +1,12 @@
+new juxtapose.JXSlider(
+  "#juxtapose-container",
+  [
+    { src: "/assets/pie-dough/not-quite.webp", label: "not quite" },
+    { src: "/assets/pie-dough/perfect.webp", label: "perfect!" },
+  ],
+  {
+    showLabels: true,
+    startingPosition: "50%",
+    makeResponsive: true,
+  }
+);
